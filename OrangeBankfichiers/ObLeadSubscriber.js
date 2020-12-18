5b197bcbc82338cde4af0fd3db5c1746f03fb7b0
ANIORT Vincent TGI/OLS <SLRQ4050@orange.com>
2020-12-18 17:49:50 +0100

    Committer: ANIORT Vincent TGI/OLS <SLRQ4050@orange.com>  On branch master  Changes to be committed: 	new file:   OrangeBankfichiers.htm 	new file:   OrangeBankfichiers/ObLeadSubscriber.js 	new file:   OrangeBankfichiers/assets-pack-premium_0.svg 	new file:   OrangeBankfichiers/assistance_0.svg 	new file:   OrangeBankfichiers/avatar.svg 	new file:   OrangeBankfichiers/check-circle.svg 	new file:   OrangeBankfichiers/compte.gif 	new file:   OrangeBankfichiers/control-center.svg 	new file:   OrangeBankfichiers/css_W5ACp9ENNIGytPAB-6J4YgruoU5vFQ6lhv1prm3LA4A.css 	new file:   OrangeBankfichiers/css_ezYDjXgk6r_S6bvT3xS5uHweAsV9hJ2rYiZuYj7MMIw.css 	new file:   OrangeBankfichiers/group-2_0.png 	new file:   OrangeBankfichiers/group-3.png 	new file:   OrangeBankfichiers/group-3_carte-standard.png 	new file:   OrangeBankfichiers/gtm.js 	new file:   OrangeBankfichiers/icons-solaris-assistance-hover.svg 	new file:   OrangeBankfichiers/icons-solaris-avatar-hover_0.svg 	new file:   OrangeBankfichiers/js_14XhhWXF5uKUv8ExF98DoVLSSRFXArkNjZ88Psu3nuY.js 	new file:   OrangeBankfichiers/js_97yvis5tBMgH9F062ZoGFWbz9aaBRyC7NtPApXGiQ8E.js 	new file:   OrangeBankfichiers/js_B52TU_weZFF6bOOaEdgC9QN5fuI-ynrFhc314hwCiYw.js 	new file:   OrangeBankfichiers/js_B7pS3ddmNLFYOJi3j28odiodelMu-EhaOeKlHZ8E6y0.js 	new file:   OrangeBankfichiers/js_BKcMdIbOMdbTdLn9dkUq3KCJfIKKo2SvKoQ1AnB8D-g.js 	new file:   OrangeBankfichiers/ob-chat-tiny.js 	new file:   OrangeBankfichiers/ob-cookie-banner.js 	new file:   OrangeBankfichiers/payment-visuals-credit-cards-new-visuals-mastercard-tiny-cla.svg 	new file:   OrangeBankfichiers/payment-visuals-credit-cards-new-visuals-mastercard-tiny-pre.svg 	new file:   OrangeBankfichiers/real-time.svg 	new file:   tests-CONCEPTEUR.xlsx

---
+++ OrangeBankfichiers/ObLeadSubscriber.js
@@ -0,0 +1,118 @@
+var expires = new Date();
+var date = new Date();
+
+expires.setTime(Date.now()+(30*24*60*60*1000));
+date.setTime(Date.now());
+
+handleCookieRules();
+document.addEventListener('ob-cookies-set', function() {
+  console.log('changed');
+  handleCookieRules();
+});
+
+function handleCookieRules() {
+  var visits = getCookie('numberOfVisits');
+  var params = getAllUrlParams();
+  if (visits) {
+    visits++;
+    document.cookie = "numberOfVisits="+visits+";expires="+expires;
+  }
+
+  if (params.of_medium) {
+    setOFCookie(params);
+  }
+}
+
+function setOFCookie(params) {
+  document.cookie = "of_medium="+params.of_medium+";expires="+expires;
+  if (params.of_campaign) {
+    document.cookie = "of_campaign="+params.of_campaign+";expires="+expires;
+  }
+  if (params.of_source) {
+    document.cookie = "of_source="+params.of_source+";expires="+expires;
+  }
+}
+
+function getAllUrlParams(url) {
+
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i = 0; i < arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // set parameter name and value (use 'true' if empty)
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+
+        // create key if it doesn't exist
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          // get the index value and add the entry at the appropriate position
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          // otherwise add the value to the end of the array
+          obj[key].push(paramValue);
+        }
+      } else {
+        // we're dealing with a string
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          // otherwise add the property
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+
+  return obj;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+
